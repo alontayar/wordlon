e74546d2d311f676f1f58406f487eb73e19ee95e
alontayar <alontayar@gmail.com>
2025-10-05 09:13:30 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('wordlon-v2').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./words-5-clean.txt'])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
