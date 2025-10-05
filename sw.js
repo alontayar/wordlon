c1cfe92775f2be7a51bc4bbb7b1b186de1c16da2
alontayar <alontayar@gmail.com>
2025-10-05 09:22:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('wordlon-v5').then(c=>c.addAll([
+    './','./index.html','./manifest.webmanifest','./words-5-clean.txt',
+    './Normal0.93-Regular.woff','./Normal0.93-Medium.woff','./Normal0.93-Bold.woff'
+  ])));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
